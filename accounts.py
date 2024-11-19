d1eb204e19d410c25709877d4d3761edf00cd0b2
Synthetiser25 <jonas@kovamees.com>
2024-11-19 17:05:59 -0500

    Wawa

--- accounts.py
+++ accounts.py
@@ -1,5 +1,5 @@
 accounts = {
-    "Account 1": 1500.75,  # Ask professor if we are allowed to stack dicts
+    68787: {"username": "waaw", "password": "ICouldSomething123", "balance": 2398.34},  # Ask professor if we are allowed to stack dicts
     "Account 2": 2375.50,
     "Account 3": 480.00,
 }
